d2298d366cdc9776ddac194175d118e52bcbcbf9
JeetDas5 <jeet15083011@gmail.com>
2024-12-24 15:32:39 +0530

    sidebar creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  MdKeyboardArrowDown,
+  MdGroups,
+  MdEventAvailable,
+} from "react-icons/md";
+import { IoIosSettings } from "react-icons/io";
+import { FiAlignJustify } from "react-icons/fi";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full h-full flex">
+        <div className="w-1/5 h-screen bg-blue-800 text-white ">
+          <div className="border-b-[1px] h-[10vh] p-2 mx-2">
+            <div className="flex flex-row items-center justify-between ">
+              <div className="flex flex-row items-center">
+                Admin
+                <MdKeyboardArrowDown className="mt-1" />
+              </div>
+              <div>
+                <IoIosSettings />
+              </div>
+            </div>
+            <p className="text-xs font-thin text-gray-400 ">
+              admin: admin@email.com
+            </p>
+          </div>
+          <div className="w-full h-[15vh] p-2">
+            <div className="flex flex-row justify-center gap-2 items-center py-2 px-2 rounded-lg hover:bg-blue-400">
+              <MdGroups />
+              <p>Societies</p>
+            </div>
+            <div className="flex flex-row justify-center gap-2 items-center py-2 px-2 rounded-lg hover:bg-blue-400">
+              <MdEventAvailable />
+              <p>Events</p>
+            </div>
+            <div className="flex flex-row justify-center gap-2 items-center py-2 px-2 rounded-lg hover:bg-blue-400">
+              <FiAlignJustify />
+              <p>Others</p>
+            </div>
+          </div>
+        </div>
+        <div className="w-4/5 h-full border border-green-500 m-2 p-2">
+          Admin Panel
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
